4b232cc11e174b0371c48b49b2beee746cd40771
Alexx1257 <alejandromacias1257@gmail.com>
2025-12-02 19:55:40 -0600

    V4-Cambios

--- src/pages/AdminDashboard/AdminDashboard.jsx
+++ src/pages/AdminDashboard/AdminDashboard.jsx
@@ -22,7 +22,7 @@ import {
 import { formatPrice } from '../../utils/formatters'
 import { getProductImage } from '../../utils/imageUtils'
 import { SIZES, PRODUCT_CATEGORIES } from '../../constants/product' 
-import { validateImageFile, validateRequired } from '../../helpers/validation' 
+import { validateImageFile, validateProductData } from '../../helpers/validation' 
 import { useErrorHandler } from '../../hooks/useErrorHandler'
 import './AdminDashboard.css'
 
